806387122a93e7b9a645012e16e0a0b08f1d677e
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-26 15:54:28 +0200

    updated C2PP comments

--- samples/SelectDirectoryConception/SelectDirectoryVCL.dpr
+++ samples/SelectDirectoryConception/SelectDirectoryVCL.dpr
@@ -1,38 +1,38 @@
-﻿/// <summary>
-/// ***************************************************************************
-///
-/// Delphi VCL Extend Library
-///
-/// Copyright 2021-2024 Patrick Prémartin under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Set of VCL components for Delphi Windows projects.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-///      Patrick PREMARTIN
-///
-/// Site :
-///      https://vclextend.developpeur-pascal.fr/
-///
-/// Project site :
-///      https://github.com/DeveloppeurPascal/Delphi-VCLExtend-Library
-///
-/// ***************************************************************************
-/// File last update : 28/05/2024 12:19:00
-/// Signature : c12bd76173d075ecc1604fda29762f458e94aae2
-/// ***************************************************************************
-/// </summary>
+﻿(* C2PP
+  ***************************************************************************
+
+  Delphi VCL Extend Library
+
+  Copyright 2021-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Set of VCL components for Delphi Windows projects.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://vclextend.developpeur-pascal.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Delphi-VCLExtend-Library
+
+  ***************************************************************************
+  File last update : 2025-02-09T11:03:48.263+01:00
+  Signature : aa45ee4e7c611861f8abd4efef5498dfcd1b2c3a
+  ***************************************************************************
+*)
 
 program SelectDirectoryVCL;
 
--- src/Olf.VCL.SelectDirectory.pas
+++ src/Olf.VCL.SelectDirectory.pas
@@ -1,38 +1,38 @@
-﻿/// <summary>
-/// ***************************************************************************
-///
-/// Delphi VCL Extend Library
-///
-/// Copyright 2021-2024 Patrick Prémartin under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Set of VCL components for Delphi Windows projects.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-///      Patrick PREMARTIN
-///
-/// Site :
-///      https://vclextend.developpeur-pascal.fr/
-///
-/// Project site :
-///      https://github.com/DeveloppeurPascal/Delphi-VCLExtend-Library
-///
-/// ***************************************************************************
-/// File last update : 28/05/2024 12:19:00
-/// Signature : 6e96232e182b5d263f2fbbd1b33bbbbf45bd40c2
-/// ***************************************************************************
-/// </summary>
+﻿(* C2PP
+  ***************************************************************************
+
+  Delphi VCL Extend Library
+
+  Copyright 2021-2025 Patrick Prémartin under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Set of VCL components for Delphi Windows projects.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://vclextend.developpeur-pascal.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Delphi-VCLExtend-Library
+
+  ***************************************************************************
+  File last update : 2025-02-09T11:03:48.263+01:00
+  Signature : bdba975cca2ef69538e71ecc81a3299dd8651d83
+  ***************************************************************************
+*)
 
 unit Olf.VCL.SelectDirectory;
 
